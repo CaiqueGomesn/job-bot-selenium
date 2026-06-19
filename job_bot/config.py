cfb76822ac4beb7382a2f4e80d5f8ff6bf486793
Caique Gomes <caique2008gomes@gmail.com>
2026-06-18 23:24:57 -0300

    Add config module

---
+++ job_bot/config.py
@@ -0,0 +1,16 @@
+PALAVRAS_CHAVE = [
+    "analista logistica",
+    "WMS",
+    "supply chain",
+    "operacoes logisticas",
+    "analista de operacoes"
+]
+
+LOCALIZACAO = "Rio de Janeiro, RJ"
+
+EMAIL = ""
+SENHA = ""
+
+MAX_CANDIDATURAS = 20
+
+MODO_TESTE = True
